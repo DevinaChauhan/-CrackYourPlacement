1011f9d04b3117474a5e2dcb406f6c81ea9db486
Devina Chauhan <143505897+DevinaChauhan@users.noreply.github.com>
2024-07-21 14:29:34 +0530

    Create combination sum 2.java

---
+++ Backtracking/combination sum 2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        ArrayList<ArrayList<Integer>> result=new ArrayList<>();
+        ArrayList<Integer> combination=new ArrayList<>();
+        int start=0;
+        Helper(candidates,target,result,combination,start);
+        return result;
+    }
+
+    public void Helper(int[] candidates, int target,ArrayList<ArrayList<Integer>> result,ArrayList<Integer> combination,int start)
+    {
+       
+       if(target<0)
+       return;
+
+      else  if(target==0)
+       result.add(new ArrayList<>(comb));
+
+       else
+       {
+           for(int i=start;i<candidates.length;i++)
+           {
+            combination.add(candidates[i]);
+            Helper(candidates,target-candidates[i],result,combination,i+1);
+            combination.remove(combination.size()-1);
+           }
+       }
+    }
+}
