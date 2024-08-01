e739d68ae252748d8b6bdfba22f166aa7fb01721
Devina Chauhan <143505897+DevinaChauhan@users.noreply.github.com>
2024-08-01 20:59:30 +0530

    Create group anagrams.java

---
+++ Strings/group anagrams.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map=new HashMap<>();
+
+        for(String s: strs){
+            
+           char[] arr=s.toCharArray();
+           Arrays.sort(arr);
+
+           String sortedstring=String.valueOf(arr);
+
+           if(!map.containsKey(sortedstring))
+           {
+            map.put(sortedstring,new ArrayList<>());
+           }
+             map.get(sortedstring).add(s);
+        }
+         return new ArrayList<>(map.values());
+    }
+}
